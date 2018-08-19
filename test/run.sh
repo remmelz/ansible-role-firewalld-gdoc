47abaada49b640915c85efd5b9d889ca1236a6b6
Kees <kees@fastmail.com>
2018-08-19 21:39:54 +0200

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -4,8 +4,8 @@ zypper -n install git ansible
 
 mkdir /etc/ansible/roles
 cd /etc/ansible/roles || exit 1
-git clone https://github.com/remmelz/ansible-role-firewalld-googledoc
+git clone https://github.com/remmelz/ansible-role-firewalld-gsync
 
-cd ansible-role-firewalld-googledoc || exit 1
+cd ansible-role-firewalld-gsync || exit 1
 ansible-playbook -c local -i '127.0.0.1,' ./tests/run.yml
 
